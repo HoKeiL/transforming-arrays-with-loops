d8b18ef6368ccf030116b59905f1551911ff38da
Your Name <you@example.com>
2023-06-26 00:37:46 +0100

    completed Exercise 1a

---
+++ 1a-greetings.js
@@ -0,0 +1,22 @@
+/**
+ * Write a function that takes an array of names, and returns an array with a greeting for each of those names: "Hello, <name>!".
+ * 
+ * 
+ * 
+ *  function name: greetingPlusNames
+ * parameters:
+ * - NAMES: array of strings
+ *  returns: array of strings
+ */
+
+function greetingPlusNames(names) {
+	const newArr = [];
+
+	for (let name of names) {
+		newArr.push("Hello, " + [name] + "!");
+	}
+
+	return newArr;
+}
+
+console.log(greetingPlusNames(["Ash", "Beth", "Ciara"]), ["Hello, Ash!", "Hello, Beth!", "Hello, Ciara!"])
