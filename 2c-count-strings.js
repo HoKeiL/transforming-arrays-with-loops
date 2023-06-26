7cba341d38bf8b3032dffb7a495b823f541a1c10
Your Name <you@example.com>
2023-06-26 14:04:56 +0100

    completed ex 2c

---
+++ 2c-count-strings.js
@@ -0,0 +1,20 @@
+/**
+ * Write a function that takes an array of strings, and returns an array with the number of characters in each string.
+ * 
+ * function: charCountArray
+ * param: arr
+ * return arr of number
+ */
+
+function charCount(str){
+	
+	return charlength = str.length;
+
+}
+
+function charCountAll(arr) {
+	return arr.map(charCount);
+	
+}
+
+console.log(charCountAll(["one", "two", "three", "four"]) , [3, 3, 5, 4])
